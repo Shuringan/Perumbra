72528bc3370d52899dbdf975c6c535478c389627
Georgiy Zharkov <bygaga.er@yandex.ru>
2025-04-21 13:26:19 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Perumbra", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
